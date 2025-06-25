6615ef160e54dc224d1bf349a313d7e6da006ead
Nickx58 <nikhilx58@gmail.com>
2025-06-25 23:16:04 +0530

    Setup route

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function MealSlug() {
+  return (
+    <div>
+      <h1>Meal Slug</h1>
+    </div>
+  );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function MealShare() {
+  return (
+    <div>
+      <h1>Meal Share</h1>
+    </div>
+  );
+}
