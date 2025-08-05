5b1747af443d847b0101d249c9df38273ce5748b
Nickx58 <nikhilx58@gmail.com>
2025-08-05 23:44:16 +0530

    Client Components

--- components/main-header.js
+++ components/main-header.js
@@ -1,9 +1,13 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
+  const path = usePathname();
+
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
@@ -14,10 +18,22 @@ export default function MainHeader() {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href="/meals">Browse Meals</Link>
+            <Link
+              href="/meals"
+              className={path.startsWith("/meals") ? classes.active : undefined}
+            >
+              Browse Meals
+            </Link>
           </li>
           <li>
-            <Link href="/community">Foodies Community</Link>
+            <Link
+              href="/community"
+              className={
+                path.startsWith("/community") ? classes.active : undefined
+              }
+            >
+              Foodies Community
+            </Link>
           </li>
         </ul>
       </nav>
